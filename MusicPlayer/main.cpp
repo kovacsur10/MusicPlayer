217bc42b5cd2456fdffb9b884f582f58dbe34e5a
Gergely Brunda <gergelybrunda@gmail.com>
2023-06-15 17:58:55 +0200

    Basic classes

--- MusicPlayer/main.cpp
+++ MusicPlayer/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <string>
+#include <filesystem>
 #include "GraphicsEngine.h"
 
+// lesz kép, a fájl neve, de az album neve is, elõadó neve,
+
+
+enum class Extension {
+	WAV,
+	MP3,
+	OGG,
+	FLAC
+};
+
+class MusicPiece {
+public:
+
+private:
+	std::string           name;
+	Extension             extension;
+	std::filesystem::path path;
+};
+
+
+class Album {
+public:
+
+private:
+	std::string             name;
+	std::filesystem::path   path;
+	std::vector<MusicPiece> musicpieces;
+};
+
+
+
+class Musician {
+public:
+
+private:
+	std::string name;
+};
+
+
 int main() {
-  std::cout << "Csaaaaaaaaaaaaaaa" << std::endl;
+
 }
