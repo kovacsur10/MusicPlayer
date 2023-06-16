fdb8f6be07123af3695d8d8cc8aa69ec0af4491f
Gergely Brunda <gergelybrunda@gmail.com>
2023-06-16 15:23:33 +0200

    directoryExplorer implementation and class header files

--- MusicPlayer/main.cpp
+++ MusicPlayer/main.cpp
@@ -1,47 +1,28 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
+#include "Album.h"
+#include "Musician.h"
+#include "MusicPiece.h"
 #include "GraphicsEngine.h"
 
-// lesz kép, a fájl neve, de az album neve is, elõadó neve,
+// lesz kép, a fájl neve, de az album neve is, elõadó neve
 
+// albumot csinálni, könyvtárat létrehozni csak album van, benne 1.mp3 2.mp3 tesztnek, zeneszerzõ1 és ezen belül album, hogy látszódjon, hogy mibõl lesz album, még egy + könyvtár és azon belül egy kép
 
-enum class Extension {
-	WAV,
-	MP3,
-	OGG,
-	FLAC
-};
+void directoryExplorer(const std::filesystem::path& path) {
 
-class MusicPiece {
-public:
-
-private:
-	std::string           name;
-	Extension             extension;
-	std::filesystem::path path;
-};
-
-
-class Album {
-public:
-
-private:
-	std::string             name;
-	std::filesystem::path   path;
-	std::vector<MusicPiece> musicpieces;
-};
-
-
-
-class Musician {
-public:
-
-private:
-	std::string name;
-};
-
+    for (auto const& dir_entry : std::filesystem::directory_iterator{ path })
+    {
+        if (dir_entry.is_directory()) {
+            directoryExplorer(dir_entry.path());
+        }
+        else if (dir_entry.is_regular_file()) {
+            std::cout << dir_entry.path() << '\n';
+        }
+    }
+}
 
 int main() {
-
+    directoryExplorer(std::filesystem::path{ "t:\\programs\\MusicPlayer\\" });
 }
